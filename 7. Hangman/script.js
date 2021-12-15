75290ccab874e2b6b8f5005b26c9adf6e32cc824
Tanishq Kumar <66270248+tanishq20@users.noreply.github.com>
2021-12-15 20:35:20 +0530

    refactor: play again button inplemented

--- 7. Hangman/script.js
+++ 7. Hangman/script.js
@@ -1,6 +1,6 @@
 const wordEl = document.querySelector('#word')
 const wrongLettersEl = document.querySelector('#wrong-letters')
-const playAgainBtn = document.querySelector('#play-again')
+const playAgainBtn = document.querySelector('#play-button')
 const popup = document.querySelector('#popup-container')
 const notification = document.querySelector('#notification-container')
 const finalMessage = document.querySelector('#final-message')
@@ -33,8 +33,6 @@ function displayWord() {
   }
 }
 
-// displayWord()
-
 window.addEventListener('keydown', (e) => {
   if (e.keyCode >= 65 && e.keyCode <= 90) {
     const letter = e.key
@@ -57,6 +55,19 @@ window.addEventListener('keydown', (e) => {
   }
 })
 
+playAgainBtn.addEventListener('click', () => {
+  //Empty array
+  correctLetters.splice(0)
+  wrongLetters.splice(0)
+
+  selectedWord = words[Math.floor(Math.random() * words.length)]
+
+  displayWord()
+  updateWrongLettersEl()
+
+  popup.style.display = 'none'
+})
+
 function updateWrongLettersEl() {
   console.log('Updated wrong')
 }
@@ -67,6 +78,28 @@ function showNotification() {
   setTimeout(() => {
     notification.classList.remove('show')
   }, 2000)
+}
+
+function updateWrongLettersEl() {
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+  `
+
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length
+
+    if (index < errors) {
+      part.style.display = 'block'
+    } else {
+      part.style.display = 'none'
+    }
+  })
+
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = 'Unfortunately you lost.'
+    popup.style.display = 'flex'
+  }
 }
 
 displayWord()
