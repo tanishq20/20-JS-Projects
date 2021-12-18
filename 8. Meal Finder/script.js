c7c63a8bfc28b82e1760b62b3117b04ac54511ac
Tanishq Kumar <66270248+tanishq20@users.noreply.github.com>
2021-12-19 01:05:39 +0530

    refactor: fetched data from mealDb api

--- 8. Meal Finder/script.js
+++ 8. Meal Finder/script.js
@@ -16,9 +16,30 @@ function searchMeal(e) {
   console.log(term)
 
   if (term.trim()) {
-    fetch(`www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        console.log(data)
+        resultHeading.innerHTML = `<h2>Search Results for '${term}':</h2>`
+
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<p>There are no serach results. Try again!</p>`
+        } else {
+          mealsEl.innerHTML = data.meals
+            .map(
+              (meal) =>
+                `<div class='meal'>
+                <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                <div class="meal-info" data-mealID="${meal.idMeal}">
+                    <h3>${meal.strMeal}</h3>
+                </div>
+              </div>`
+            )
+            .join('')
+        }
+      })
+
+    search.value = ''
   } else {
     alert('Please enter a search term')
   }
