d08ff8fe2625143b00c45d99c7dc86867b3b953c
Tanishq Kumar <66270248+tanishq20@users.noreply.github.com>
2021-12-23 01:20:31 +0530

    refactor: added localstorage to project

--- 9. Expense Tracker/script.js
+++ 9. Expense Tracker/script.js
@@ -1,36 +1,78 @@
-const balance = document.querySelector('#balance')
-const moneyPlus = document.querySelector('#money-plus')
-const moneyMinus = document.querySelector('#money-minus')
-const list = document.querySelector('#list')
-const form = document.querySelector('#form')
-const text = document.querySelector('#text')
-const amount = document.querySelector('#amount')
+const balance = document.getElementById('balance')
+const money_plus = document.getElementById('money-plus')
+const money_minus = document.getElementById('money-minus')
+const list = document.getElementById('list')
+const form = document.getElementById('form')
+const text = document.getElementById('text')
+const amount = document.getElementById('amount')
 
-const dummyTransactions = [
-  { id: 1, text: 'Flower', amount: -20 },
-  { id: 2, text: 'Salary', amount: 300 },
-  { id: 3, text: 'Book', amount: -10 },
-  { id: 4, text: 'Camera', amount: 150 },
-]
+// const dummyTransactions = [
+//   { id: 1, text: 'Flower', amount: -20 },
+//   { id: 2, text: 'Salary', amount: 300 },
+//   { id: 3, text: 'Book', amount: -10 },
+//   { id: 4, text: 'Camera', amount: 150 }
+// ];
 
-let transactions = dummyTransactions
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem('transactions')
+)
 
+let transactions =
+  localStorage.getItem('transactions') !== null ? localStorageTransactions : []
+
+// Add transaction
+function addTransaction(e) {
+  e.preventDefault()
+
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert('Please add a text and amount')
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value,
+    }
+
+    transactions.push(transaction)
+
+    addTransactionDOM(transaction)
+
+    updateValues()
+
+    updateLocalStorage()
+
+    text.value = ''
+    amount.value = ''
+  }
+}
+
+// Generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 100000000)
+}
+
+// Add transactions to DOM list
 function addTransactionDOM(transaction) {
+  // Get sign
   const sign = transaction.amount < 0 ? '-' : '+'
 
   const item = document.createElement('li')
 
+  // Add class based on value
   item.classList.add(transaction.amount < 0 ? 'minus' : 'plus')
 
   item.innerHTML = `
     ${transaction.text} <span>${sign}${Math.abs(
     transaction.amount
-  )}</span> <button class="delete-btn">X</button>
+  )}</span> <button class="delete-btn" onclick="removeTransaction(${
+    transaction.id
+  })">x</button>
   `
 
   list.appendChild(item)
 }
 
+// Update the balance, income and expense
 function updateValues() {
   const amounts = transactions.map((transaction) => transaction.amount)
 
@@ -46,15 +88,33 @@ function updateValues() {
     -1
   ).toFixed(2)
 
-  balance.innerText = `₹${total}`
-  moneyPlus.innerText = `₹${income}`
-  moneyMinus.innerText = `₹${expense}`
+  balance.innerText = `$${total}`
+  money_plus.innerText = `$${income}`
+  money_minus.innerText = `$${expense}`
 }
 
+// Remove transaction by ID
+function removeTransaction(id) {
+  transactions = transactions.filter((transaction) => transaction.id !== id)
+
+  updateLocalStorage()
+
+  init()
+}
+
+// Update local storage transactions
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions))
+}
+
+// Init app
 function init() {
   list.innerHTML = ''
+
   transactions.forEach(addTransactionDOM)
   updateValues()
 }
 
 init()
+
+form.addEventListener('submit', addTransaction)
